4b7428cc91adf5f788afcb4af2c03d4745fc944b
Thomas Farias <thomasfarias0995@gmail.com>
2023-10-23 22:34:22 -0300

    Teste de Login

---
+++ OneDrive/Área de Trabalho/Exercicios/projeto/profile/profile/src/App.jsx
@@ -0,0 +1,17 @@
+import Profile from "./componets/Profile";
+
+export default function App() {
+  return(
+    <div className="app">
+      <Profile
+      avatar="https://avatars.githubusercontent.com/u/98232934?v=4"
+      name="Thomas Farias"
+      bio="Em transição de area. Desenvolvimento de software/web"
+      email="thomasfarias0995@gmail.com"
+      phone="(081) 9 9826-4250"
+      githubUrl="https://github.com/thomasfarias95"
+      linkedinUrl="https://www.linkedin.com/in/thomas-farias-13b865169/"
+      />
+    </div>
+  )
+}
---
+++ OneDrive/Área de Trabalho/Exercicios/projeto/profile/profile/src/componets/Profile/index.jsx
@@ -0,0 +1,17 @@
+import styles from "./styles.module.css"
+
+export default function Profile(props) {
+    return (
+        <div className={styles.container}>
+            <img className={styles.avatar} src={props.avatar} alt={props.name} />
+            <h2>{props.name}</h2>
+            <div>{props.bio}</div>
+            <div>{props.phone}</div>
+            <div>{props.email}</div>
+            <div className={styles.links}>
+                <a href={props.githubUrl} target="_blank">GitHub</a>
+                <a href={props.linkedinUrl}target="_blank">Linkedin</a>
+            </div>
+        </div>
+    )
+}
